c2cdf87231f299f73ab5555787daca96a5af5332
Dan Guest <dguest@cern.ch>
2016-02-16 00:03:41 +0100

    Extend NanReplacer to also replace inf / neg inf

--- include/NanReplacer.hh
+++ include/NanReplacer.hh
@@ -5,14 +5,25 @@
 #include <string>
 
 namespace lwt {
+  namespace rep {
+    const unsigned nan  = 0x1 << 0;
+    const unsigned inf  = 0x1 << 1;
+    const unsigned ninf = 0x1 << 2;
+    const unsigned all  = nan | inf | ninf;
+  }
   class NanReplacer
   {
   public:
     typedef std::map<std::string, double> ValueMap;
-    NanReplacer(const ValueMap& defaults);
+    NanReplacer(const ValueMap& defaults, int flags = rep::nan);
+    NanReplacer(NanReplacer&) = delete;
+    NanReplacer& operator=(NanReplacer&) = delete;
     ValueMap replace(const ValueMap& in) const;
   private:
-    ValueMap _defaults;
+    ValueMap m_defaults;
+    bool m_do_nan;
+    bool m_do_inf;
+    bool m_do_ninf;
   };
 }
 
--- src/NanReplacer.cxx
+++ src/NanReplacer.cxx
@@ -7,9 +7,13 @@ static_assert(std::numeric_limits<double>::has_quiet_NaN,
               "no NaN defined, but we require one");
 
 namespace lwt {
-  NanReplacer::NanReplacer(const NanReplacer::ValueMap& defaults):
-    _defaults(defaults)
-  {}
+  NanReplacer::NanReplacer(const NanReplacer::ValueMap& defaults, int fg):
+    m_defaults(defaults),
+    m_do_nan(fg & rep::nan),
+    m_do_inf(fg & rep::inf),
+    m_do_ninf(fg & rep::ninf)
+  {
+  }
 
   NanReplacer::ValueMap
   NanReplacer::replace(const NanReplacer::ValueMap& inputs) const {
@@ -18,10 +22,19 @@ namespace lwt {
 
     // loop over all inputs
     for (const auto& in: inputs) {
-      if (std::isnan(in.second) && _defaults.count(in.first)) {
-        outputs[in.first] = _defaults.at(in.first);
+      double val = in.second;
+
+      // figure out if this value should be replaced
+      bool is_nan = m_do_nan && std::isnan(val);
+      bool is_inf = m_do_inf && std::isinf(val) && !std::signbit(val);
+      bool is_ninf = m_do_ninf && std::isinf(val) && std::signbit(val);
+      bool is_bad = is_nan || is_inf || is_ninf;
+
+      bool in_defaults = m_defaults.count(in.first);
+      if (is_bad && in_defaults) {
+        outputs[in.first] = m_defaults.at(in.first);
       } else {
-        outputs[in.first] = in.second;
+        outputs[in.first] = val;
       }
     }
     return outputs;
