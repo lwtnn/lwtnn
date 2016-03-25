c73e9c9e1457bd429afde3e3fb85ec69cfc22fe6
Dan Guest <dguest@cern.ch>
2016-03-25 23:23:44 +0100

    Add maxout layer converter

--- converters/keras2json.py
+++ converters/keras2json.py
@@ -5,19 +5,19 @@
 ____________________________________________________________________
 Variable specification file
 
-In additon to the standard Keras archetecture and weights files, you
+In additon to the standard Keras architecture and weights files, you
 must provide a "variable specification" json file with the following
 format:
 
   {
-    {"inputs": [
+    "inputs": [
       {"name": variable_name,
        "scale": scale,
        "offset": offset,
        "default": default_value},
       ...
-      ] }
-    {"class_labels": [output_class_1_name, output_class_2_name, ...] }
+      ],
+    "class_labels": [output_class_1_name, output_class_2_name, ...]
   }
 
 where `scale` and `offset` account for any scaling and shifting to the
@@ -39,7 +39,7 @@ def _run():
         inputs = json.load(inputs_file)
     with h5py.File(args.hdf5_file, 'r') as h5:
         out_dict = {
-            'layers': _get_layers(arch, h5),
+            'layers': _get_layers(arch, inputs, h5),
         }
         out_dict.update(_parse_inputs(inputs))
     print(json.dumps(out_dict, indent=2))
@@ -57,6 +57,12 @@ def _get_args():
 _activation_map = {
     'relu': 'rectified',
     'sigmoid': 'sigmoid',
+    None: 'linear',
+    # TODO: pass through unknown types rather than defining them as
+    # themselves?
+    'linear': 'linear',
+    'softmax': 'softmax',
+    'tanh': 'tanh',
 }
 
 # __________________________________________________________________________
@@ -88,20 +94,46 @@ def _get_dense_layer_parameters(layer_group, n_in):
 
 def _get_maxout_layer_parameters(layer_group, n_in):
     """Get weights, bias, and n-outputs for a maxout layer"""
-    raise NotImplementedError
+    weights = np.asarray(layer_group['param_0'])
+    bias = np.asarray(layer_group['param_1'])
+
+    # checks (note the transposed arrays)
+    wt_layers, wt_in, wt_out = weights.shape
+    bias_layers, bias_n = bias.shape
+    assert wt_out == bias_n
+    assert wt_in == n_in, '{} != {}'.format(wt_in, n_in)
+    assert wt_layers == bias_layers
+
+    sublayers = []
+    for nnn in range(weights.shape[0]):
+        w_slice = weights[nnn,:,:]
+        b_slice = bias[nnn,:]
+        sublayer = {
+            'weights': w_slice.T.flatten().tolist(),
+            'bias': b_slice.flatten().tolist(),
+            'architecture': 'dense'
+        }
+        sublayers.append(sublayer)
+    return {'sublayers': sublayers, 'architecture': 'maxout'}, wt_out
+
+def _dummy_parameters(layer_group, n_in):
+    """Return dummy parameters"""
+    return {'weights':[], 'bias':[], 'architecture':'dense'}, n_in
 
 _layer_converters = {
     'dense': _get_dense_layer_parameters,
-    'maxout': _get_maxout_layer_parameters,
+    'maxoutdense': _get_maxout_layer_parameters,
+    'activation': _dummy_parameters,
+    'flatten': _dummy_parameters,
     }
 
 # __________________________________________________________________________
 # master layer converter / inputs function
 
-def _get_layers(network, h5):
+def _get_layers(network, inputs, h5):
     layers = []
     in_layers = network['layers']
-    n_out = h5['layer_0']['param_0'].shape[0]
+    n_out = len(inputs['inputs'])
     for layer_n in range(len(in_layers)):
         # get converter for this layer
         layer_arch = in_layers[layer_n]
@@ -113,7 +145,8 @@ def _get_layers(network, h5):
 
         # build the out layer
         out_layer, n_out = convert(layer_group, n_out)
-        out_layer['activation'] = _activation_map[layer_arch['activation']]
+        out_layer['activation'] = _activation_map[
+            layer_arch.get('activation')]
         layers.append(out_layer)
     return layers
 
