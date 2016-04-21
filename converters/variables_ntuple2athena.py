28fc55ff7dcd51bf0abac0ebe8f02b303aac9317
Marie89 <marie.lanfermann@cern.ch>
2016-04-21 12:50:18 +0200

    add output label conversion
    to match the label names used outside Athena to the ones used by DL1
    inside Athena

--- converters/variables_ntuple2athena.py
+++ converters/variables_ntuple2athena.py
@@ -48,12 +48,19 @@ _variable_convention_converter = {
     'sv1_sig3': 'sv1_sig3d',
     }
 
+_output_labeling_convention_converter = {
+    'b-jet': 'bottom',
+    'c-jet': 'charm',
+    'u-jet': 'light'
+}
+
 def _update_naming_convention(network):
     """Update naming convention from the one used during training to the one used in Athena"""
-    input_variable_list = _get_input_variables(network)
+    input_variables_list = _get_input_variables(network)
+    output_variables_list = _get_output_variables(network)
 
     # replace 'default' key string with the one used in Athena:
-    for variable_name in input_variable_list:
+    for variable_name in input_variables_list:
         if variable_name in list(network['defaults'].keys()):
             if variable_name in list(_variable_convention_converter.keys()):
                 network['defaults'][_variable_convention_converter.get(variable_name)] = network['defaults'].get(variable_name)
@@ -63,6 +70,12 @@ def _update_naming_convention(network):
     for variable_itr, variable_item in enumerate(network.get('inputs')):
         if variable_item.get('name') in list(_variable_convention_converter.keys()):
             network['inputs'][variable_itr]['name'] = _variable_convention_converter.get(variable_item.get('name'))
+
+    # replace 'output' label string with the one used in Athena:
+    for variable_itr, variable_name in enumerate(output_variables_list):
+        if variable_name in list(_output_labeling_convention_converter.keys()):
+            network['outputs'][variable_itr] = _output_labeling_convention_converter.get(variable_name)
+
     return network
 
 def _get_input_variables(network):
@@ -70,6 +83,12 @@ def _get_input_variables(network):
     for variable in network.get('inputs'):
         input_variables.append(variable.get('name'))
     return input_variables
+
+def _get_output_variables(network):
+    output_variables = []
+    for variable in network.get('outputs'):
+        output_variables.append(variable)
+    return output_variables
 
 if __name__ == '__main__':
     _run()
