7367421d34bb05056aa051f101ee7ef8b7b61aef
Dan Guest <dguest@cern.ch>
2016-04-28 16:45:11 +0200

    Add LSTM components to LayerConfig

--- include/lwtnn/NNLayerConfig.hh
+++ include/lwtnn/NNLayerConfig.hh
@@ -10,20 +10,24 @@
 
 #include <vector>
 #include <string>
+#include <map>
 
 namespace lwt {
   enum class Activation {LINEAR, SIGMOID, RECTIFIED, SOFTMAX, TANH};
-  enum class Architecture {DENSE, MAXOUT};
+  enum class Architecture {DENSE, MAXOUT, LSTM};
+  enum class Component {I,O,C,F};
 
   struct LayerConfig
   {
     // dense layer info
     std::vector<double> weights;
     std::vector<double> bias;
+    std::vector<double> U;      // TODO: what is this thing called in LSTMs?
     Activation activation;
 
     // additional info for sublayers
     std::vector<LayerConfig> sublayers;
+    std::map<Component, LayerConfig> components;
 
     // arch flag
     Architecture architecture;
