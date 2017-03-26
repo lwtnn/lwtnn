6d6c2b09d482f92d319b1f899d0a259e1adda985
Dan Guest <dguest@cern.ch>
2017-03-27 00:03:43 +0200

    Add graph model test, confirm against Keras
    Added the last parts of the Keras functional -> lwtnn test chain: we
    can now make an arbitrary Keras graph (subject to some constraints
    since only a few node types have been implemented), save it to JSON,
    and then execute the network in C++.
    
    Several test patterns confirmed that the output is identical to Keras.

--- src/lwtnn-test-graph.cxx
+++ src/lwtnn-test-graph.cxx
@@ -1,27 +1,52 @@
 #include "lwtnn/Graph.hh"
+#include "lwtnn/parse_json.hh"
 #include "lwtnn/NNLayerConfig.hh"
 
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-  size_t node_number = 1;
-  if (argc > 1) node_number = atoi(argv[1]);
+#include <unistd.h>
+
+lwt::GraphConfig dummy_config() {
   using namespace lwt;
+  GraphConfig config;
+  std::vector<Input> dummy_inputs{ {"one", 0, 1}, {"two", 0, 1} };
+  config.inputs = {{"one", dummy_inputs}, {"two", dummy_inputs}};
   typedef NodeConfig::Type Type;
-  DummySource source({2,2});
-  std::vector<NodeConfig> nodes;
-  nodes.push_back({Type::INPUT, {0}, 2});
-  nodes.push_back({Type::INPUT, {1}, 2});
-  nodes.push_back({Type::CONCATENATE, {0, 1}});
-  nodes.push_back({Type::FEED_FORWARD, {2}, 0});
-  nodes.push_back({Type::FEED_FORWARD, {3}, 0});
+  config.nodes.push_back({Type::INPUT, {0}, 2});
+  config.nodes.push_back({Type::INPUT, {1}, 2});
+  config.nodes.push_back({Type::CONCATENATE, {0, 1}});
+  config.nodes.push_back({Type::FEED_FORWARD, {2}, 0});
+  config.nodes.push_back({Type::FEED_FORWARD, {3}, 0});
 
   LayerConfig dense {
     {0, 0, 0, 1,  0, 0, 1, 0,  0, 1, 0, 0,  1, 0, 0, 0}};
   dense.activation = Activation::LINEAR;
   dense.architecture = Architecture::DENSE;
+  config.layers.push_back(dense);
+  return config;
+}
 
-  lwt::Graph graph(nodes, {dense});
-  std::cout << graph.compute(source, node_number) << std::endl;
+int main(int argc, char* argv[]) {
+  lwt::GraphConfig config;
+  if (isatty(fileno(stdin))) {
+    config = dummy_config();
+  } else {
+    config = lwt::parse_json_graph(std::cin);
+  }
+  int node_number = -1;
+  if (argc > 1) node_number = atoi(argv[1]);
+  using namespace lwt;
+  std::vector<size_t> inputs_per_node;
+  for (const auto& innode: config.inputs) {
+    inputs_per_node.push_back(innode.variables.size());
+  }
+  DummySource source(inputs_per_node);
+
+  lwt::Graph graph(config.nodes, config.layers);
+  if (node_number < 0) {
+    std::cout << graph.compute(source) << std::endl;
+  } else {
+    std::cout << graph.compute(source, node_number) << std::endl;
+  }
   return 0;
 }
