2798b103fac9549a63f67e6307e7684f78d0b9c9
Dan Guest <dguest@cern.ch>
2017-07-27 21:46:49 +0200

    try new include path

--- tests/travis-build-cmake.sh
+++ tests/travis-build-cmake.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export BOOST_INC=/usr/local/include/boost
-export EIGEN_INC=/usr/local/include/eigen3
+export BOOST_INC=/usr/include/boost
+export EIGEN_INC=/usr/include/eigen3
 mkdir build
 pushd .
 cd build
