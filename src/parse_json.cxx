68213219070f966ec268e66dfa978fb7ccc2df2f
aighosh <ghosh@lal.in2p3.fr>
2018-11-05 16:15:27 +0100

    fixed swishbeta -> swish

--- src/parse_json.cxx
+++ src/parse_json.cxx
@@ -205,7 +205,7 @@ namespace {
     if (str == "hard_sigmoid") return Activation::HARD_SIGMOID;
     if (str == "elu") return Activation::ELU;
     if (str == "leakyrelu") return Activation::LEAKY_RELU;
-    if (str == "swishbeta") return Activation::SWISHBETA;
+    if (str == "swish") return Activation::SWISH;
     throw std::logic_error("activation function " + str + " not recognized");
     return Activation::LINEAR;
   }
