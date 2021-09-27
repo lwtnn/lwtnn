42485df30d9eb44298e52b805eab1f0fb18d60cc
Lauri Laatu <lauri.laatu@aalto.fi>
2021-09-27 14:45:52 +0200

    Revert to earlier version

--- tests/reg-test.py
+++ tests/reg-test.py
@@ -38,7 +38,7 @@ def _run():
         parser.print_usage()
         sys.exit('you need to pipe in a file')
 
-    parse = _get_output_node_dicts
+    parse = _get_output_node_dicts if args.graph else _get_dict
     in_dict = parse(sys.stdin)
     if not args.saved_variables:
         print(json.dumps(in_dict, indent=2, sort_keys=True))
@@ -80,6 +80,7 @@ def _get_output_node_dicts(infile):
     odict = {}
     node_key = None
     for line in infile:
+
         if line.endswith(':\n'):
             node_key = line[:-1]
             assert node_key not in odict
@@ -87,11 +88,8 @@ def _get_output_node_dicts(infile):
             continue
 
         key, *vals = line.split()
-        vals = [float(val) for val in vals]
-        if node_key is not None:
-            odict[node_key][key] = vals if len(vals) > 1 else vals[0]
-        else:
-            odict[key] = vals if len(vals) > 1 else vals[0]
+        odict[node_key][key] = [float(val) for val in vals]
+
     return odict
 
 
@@ -100,69 +98,44 @@ def _get_dict(infile):
     lines, formatted as `<key> <value>`.
     """
     odict = {}
-    first_key = None
     for line in infile:
-        try:
-            key, val = line.split()
-            odict[key] = [float(val)]
-        except ValueError:
-            # first line as key
-            first_key = line.replace("\n", "")
-        except Exception as e:
-            print(e)
-    if first_key is None:
-      return odict
-    else:
-      return {first_key: odict}
+        key, val = line.split()
+        odict[key] = [float(val)]
+    return odict
 
 
 def _compare_equal(old, new, tolerance, warn_threshold=0.0000001):
     """
     For values of x where abs(x) < 1, the threshold is absolute.
     For larger values, use a relative threshold.
-
-    Assumes inputs are of same type and of dict, list or int/float
-
     """
-    if isinstance(old, (int, float)):
-        diff = old - new
-        avg = (old + new) / 2
-        rel = abs(diff) / abs(avg) if abs(avg) > 1 else abs(diff)
-        # first do warnings
-        if rel > warn_threshold:
-            sys.stderr.write(
-              'WARNING: value is off in new version by {}\n'.format(
-               diff))
-        if rel > tolerance:
-            sys.stderr.write(
-             'ERROR: change in value is over threshold {}\n'.format(
-              tolerance))
-            return False
-        return True
-    elif isinstance(old, list):
-        if isinstance(new, (int, float)) and len(old) == 1:
-            return _compare_equal(old[0], new, tolerance)
-        correct = []
-        for o, n in zip(old, new):
-            correct.append(_compare_equal(o, n, tolerance))
-        if False in correct:
-            return False
-        else:
-            return True
-    elif isinstance(old, dict):
-        if set(old) != set(new):
-            sys.stderr.write(
-                'ERROR: variable mismatch. Targets: "{}", given "{}"\n'.format(
-                    ', '.join(old), ', '.join(new)))
-            return False
-        correct = []
-        for o_v, n_v in zip(old.values(), new.values()):
-            correct.append(_compare_equal(o_v, n_v, tolerance))
-        if False in correct:
-            return False
-        else:
-            return True
+    if set(old) != set(new):
+        sys.stderr.write(
+            'ERROR: variable mismatch. Targets: "{}", given "{}"\n'.format(
+                ', '.join(old), ', '.join(new)))
+        return False
 
+    fails = set()
+    for var in old:
+        oldseq = old[var] if isinstance(old[var], Sequence) else [old[var]]
+        for idx, oldvar in enumerate(oldseq):
+            newvar = new[var][idx]
+            diff = oldvar - newvar
+            avg = (oldvar + newvar) / 2
+            rel = abs(diff) / abs(avg) if abs(avg) > 1 else abs(diff)
+            # first do warnings
+            if rel > warn_threshold:
+                sys.stderr.write(
+                    'WARNING: "{}" is off in new version by {}\n'.format(
+                        var, diff))
+            if rel > tolerance:
+                sys.stderr.write(
+                    'ERROR: change in "{}" is over threshold {}\n'.format(
+                        var, tolerance))
+                fails.add(var)
+    if fails:
+        return False
+    return True
 
 if __name__ == '__main__':
     _run()
